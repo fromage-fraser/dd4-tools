398058a28b3c529e3af09995440f991ce1464209
Fromage Fraser <50809996+fromage-fraser@users.noreply.github.com>
2025-08-06 15:51:28 +0100

    Simplify gradle submodule configuration (#16)
    * Simplify gradle submodule configuration

--- area-query/build.gradle.kts
+++ area-query/build.gradle.kts
@@ -1,8 +1,4 @@
-import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
-
 plugins {
-    kotlin("jvm")
-    id("org.jlleitschuh.gradle.ktlint")
     application
 }
 
@@ -19,35 +15,9 @@ dependencies {
     implementation(Libs.jackson_module_kotlin)
 }
 
-kotlin {
-    jvmToolchain(21)
-}
-
-val jar by tasks.getting(Jar::class) {
+tasks.shadowJar {
     archiveFileName.set("area-query.jar")
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest {
         attributes["Main-Class"] = "dd4.areaquery.AreaQueryMainKt"
-    }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
-        exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
-    }
-}
-
-ktlint {
-    version.set("1.6.0")
-    verbose.set(true)
-    outputToConsole.set(true)
-    coloredOutput.set(true)
-
-    reporters {
-        reporter(ReporterType.CHECKSTYLE)
-        reporter(ReporterType.PLAIN)
-        reporter(ReporterType.HTML)
-    }
-
-    filter {
-        include("src/main/kotlin/**")
-        include("src/test/kotlin/**")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     kotlin("jvm") version "2.1.21"
     id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 allprojects {
@@ -9,5 +12,28 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    kotlin {
+        jvmToolchain(21)
+    }
+
+    ktlint {
+        version.set("1.6.0")
+        verbose.set(true)
+        outputToConsole.set(true)
+        coloredOutput.set(true)
+
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.HTML)
+        }
     }
 }
